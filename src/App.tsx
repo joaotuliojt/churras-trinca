3684fe629e2f706b142be9ede064d7dd0dd526ca
joaotuliojt <joaotuliosoares@hotmail.com>
2023-01-27 17:34:12 -0300

    chore: add eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
+const a = ''
+
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <h1>Hello Wolrd</h1>
-  )
+  return <h1>Hello Wolrd</h1>
 }
 
 export default App

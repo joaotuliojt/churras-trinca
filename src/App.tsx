f2408dabf26170cd1071b6afb73e2c5001a5a361
joaotuliojt <joaotuliosoares@hotmail.com>
2023-01-29 19:07:30 -0300

    refactor: set index page with login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { resetCss } from '../stitches.config'
-import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
 function App() {
   resetCss()
   return (
     <Routes>
-      <Route index element={<Home />} />
+      <Route index element={<Login />} />
     </Routes>
   )
 }

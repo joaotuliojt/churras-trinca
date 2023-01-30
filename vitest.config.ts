cfba969d0c8073e395cf3d90b7442c0dbeaa1126
joaotuliojt <joaotuliosoares@hotmail.com>
2023-01-30 00:36:47 -0300

    chore: add default vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts'
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, './src/components')
+      },
+      {
+        find: '@styles/stitches',
+        replacement: path.resolve(__dirname, './src/styles/stitches.ts')
+      }
+    ]
   }
 })

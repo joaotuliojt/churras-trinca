d9c276d23d0b095a6732603c6bca8c38c7e9354a
joaotuliojt <joaotuliosoares@hotmail.com>
2023-01-29 23:07:21 -0300

    chore: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, './src/components')
+      },
+      {
+        find: '@styles/stitches',
+        replacement: path.resolve(__dirname, './src/styles/stitches.ts')
+      }
+    ]
+  }
 })
